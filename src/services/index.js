809c6b861d388da556451ad915aa7caa60a9972a
ancorframe <yura.pundiak@gmail.com>
2022-09-04 12:21:04 +0300

    +

--- src/services/index.js
+++ src/services/index.js
@@ -1,20 +1,61 @@
+import { initializeApp } from "firebase/app";
+import { firebaseConfig } from "../firebase";
+
+import { getDatabase, ref, set, get, remove } from "firebase/database";
+
+const app = initializeApp(firebaseConfig);
+
 const TASKS = "list_of_tasks";
 
-export function addTaskToLocalStorage(task) {
-  const parsedLocalStorage = getTaskFromLocalStorage(TASKS);
-  const tasks = parsedLocalStorage ? JSON.parse(parsedLocalStorage) : [];
-  tasks.push(task);
-  addToLocalStorage(tasks);
+const db = getDatabase();
+
+export function addToFirebaseStorage(task) {
+  try {
+    set(ref(db, "tasks/" + task.id), task);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-export function getTaskFromLocalStorage(key = TASKS) {
-  return localStorage.getItem(key);
+// export function addToFirebaseStorage(task) {
+//   const parsedLocalStorage = getTaskFromLocalStorage(TASKS);
+//   const tasks = parsedLocalStorage ? JSON.parse(parsedLocalStorage) : [];
+//   tasks.push(task);
+//   addToLocalStorage(tasks);
+// }
+
+export function getTaskFromFirebaseStorage(id) {
+  const url = id ? `tasks/${id}` : "tasks/";
+  return get(ref(db, url))
+    .then((snapshot) => {
+      if (snapshot.exists()) {
+        if (id) {
+          return snapshot.val();
+        }
+        return Object.values(snapshot.val());
+      } else {
+        console.log("No data available");
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 }
+
+// export function getTaskFromLocalStorage(key = TASKS) {
+//   return localStorage.getItem(key);
+// }
 
 export function createTask(inputVal) {
   return { inputVal, isChecked: false, id: Date.now() };
 }
 
 export function addToLocalStorage(tasks) {
-    localStorage.setItem(TASKS, JSON.stringify(tasks));
+  localStorage.setItem(TASKS, JSON.stringify(tasks));
+}
+
+export function removeFromFirebase(id) {
+  try {
+    return remove(ref(db, "tasks/" + id));
+  } catch (error) {}
 }
